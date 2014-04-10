db8bc11cd61354d26aac432edd9599ad64976240
Mark Kreyman <mark@kreyman.com>
2014-04-10 13:30:01 -0600

    Removed extra specs and made FactoryGirl factory more entertaining

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,11 +1,23 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :product do
-    name { Faker::Commerce.product_name }
-    description { Faker::Lorem.sentence }
-    price { rand(0.01..999.99) }
-    image_url "https://github-images.s3.amazonaws.com/help/repository/https-url-clone.png"
+
+  FactoryGirl.define do
+    factory :product do
+      name { Faker::Commerce.product_name }
+      description {
+        description = <<-DESCRIPTION
+        These #{Faker::Commerce.color} #{name.pluralize}
+        will help you generate
+        #{Faker::Company.catch_phrase} with
+        #{Faker::Company.bs}
+        DESCRIPTION
+        description.gsub!(/[\n\s]+/, ' ')
+        description
+      }
+      image_url { "/images/#{rand(10)}.png" }
+      price { sprintf("%.02f", rand * 999) }
+    end
   end
 
 end
